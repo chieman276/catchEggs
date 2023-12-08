7d2c4efd395c56509ef6f8f30e335949da4f9c63
Mai Chiếm An <anmc@runsystem.net>
2023-12-08 16:11:50 +0700

    catch eggs

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ function endGame() {
   cancelAnimationFrame(animationFrame);
   setTimeout(function() {
     gameOver = true;
-  }, 35)
+  }, 40)
   // Hiển thị thông báo và nút tiếp tục
   var messageContainer = document.createElement("div");
   messageContainer.id = "message-container";
